849381e1a7311eb0a2b1d0f437d187824e93e179
LocBQ <locbq@bkav.com>
2023-12-21 13:53:21 +0700

    client-side

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import { Server } from "socket.io"
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const PORT = process.env.PORT || 3500
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "public")))
+
+const expressServer = app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
---
+++ public/app.js
@@ -0,0 +1,43 @@
+const socket = io('ws://localhost:3500')
+const activity = document.querySelector('.activity')
+const msgInput = document.querySelector('#message')
+const nameInput = document.querySelector('#name')
+const chatRoom = document.querySelector('room')
+const userList = document.querySelector('.user-list')
+const roomList = document.querySelector('.room-list')
+const chatDisplay = document.querySelector('.chat-display')
+
+function sendMessage(e) {
+    e.preventDefault()
+    if (nameInput.value && msgInput.value && chatRoom.value) {
+        socket.emit('message', {
+            "name": nameInput.value,
+            "text": msgInput.value
+        })
+        msgInput.value = ""
+    }
+    msgInput.focus()
+}
+
+function enterRoom(e) {
+    e.preventDefault()
+    if(nameInput.value && chatRoom.value) {
+        socket.emit('enterRoom', {
+            "name": nameInput.value,
+            "room": chatRoom.value
+        })
+    }
+}
+
+document.querySelector(".form-msg").addEventListener('submit', sendMessage)
+document.querySelector(".form-join").addEventListener('submit', enterRoom)
+msgInput.addEventListener('keypress', () => {
+    socket.emit('activity', nameInput.value)
+})
+
+socket.on("message", (data)=>{
+    activity.textContent = ""
+    const li = document.createElement('li')
+    li.textContent = data
+    document.querySelector('ul').appendChild(li)
+})
